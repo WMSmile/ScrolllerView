19c7b810b9cad38642d5e8a5af8a6dae13b8d0a2
Mac <mac@MacdeMacBook-Pro.local>
2015-04-30 15:31:03 +0800

    自定义的图片浏览，可以左右滑动的。并且有点击事件的代理方法哦

--- ScrolllerView/PhotosView.m
+++ ScrolllerView/PhotosView.m
@@ -7,7 +7,7 @@
 //
 
 #import "PhotosView.h"
-#define TimeInterval 1 //动画的间隔时间
+#define TimeInterval 3 //动画的间隔时间
 
 @interface PhotosView()<UIScrollViewDelegate>
 {
@@ -46,10 +46,10 @@
     
     
     //增加显示第几张的label
-    self.imgCountLB = [[UILabel alloc]initWithFrame:CGRectMake(15, self.frame.size.height-20, 30, 20)];
+    self.imgCountLB = [[UILabel alloc]initWithFrame:CGRectMake(5, self.frame.size.height-20, 30, 12)];
     _imgCountLB.text = @"0/0";
-    _imgCountLB.backgroundColor = [UIColor lightGrayColor];
-    _imgCountLB.font = [UIFont systemFontOfSize:10];
+    _imgCountLB.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.4];
+    _imgCountLB.font = [UIFont systemFontOfSize:9];
     _imgCountLB.textColor = [UIColor whiteColor];
     _imgCountLB.textAlignment = UITextAlignmentCenter;
     [self addSubview:_imgCountLB];
@@ -62,28 +62,32 @@
         return;
     }
     _ListAry = ListAry;
-    
-    for (int i = 0; i<ListAry.count; i++) {
-        
-        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        btn.frame = CGRectMake(self.frame.size.width * i, 0, self.frame.size.width, self.frame.size.height);
-        btn.tag = 100 + i;
-        [btn addTarget:self action:@selector(BtnsClick:) forControlEvents:UIControlEventTouchUpInside];
-        [btn setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:ListAry[i]]]] forState:UIControlStateNormal];
-        [self.imgSV addSubview:btn];
-        
-        self.imgSV.contentSize = CGSizeMake(self.frame.size.width*ListAry.count, self.frame.size.height);
+    @autoreleasepool {
+        for (int i = 0; i<ListAry.count; i++) {
+            
+            
+            UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+            btn.frame = CGRectMake(self.frame.size.width * i, 0, self.frame.size.width, self.frame.size.height);
+            btn.tag = 100 + i;
+            [btn addTarget:self action:@selector(BtnsClick:) forControlEvents:UIControlEventTouchUpInside];
+            [btn setBackgroundImageForState:UIControlStateNormal withURL:[NSURL URLWithString:ListAry[i]] placeholderImage:IMG(@"car_photo2")];
+//            [btn setImageForState:UIControlStateNormal withURL:[NSURL URLWithString:ListAry[i]] placeholderImage:nil];
+//            btn.imageView.contentMode = UIViewContentModeScaleToFill;
+            [self.imgSV addSubview:btn];
+            
+        }
+
     }
+
+    self.imgSV.contentSize = CGSizeMake(self.frame.size.width*ListAry.count, self.frame.size.height);
     _totalCount = ListAry.count;
-    _imgCountLB.text = [NSString stringWithFormat:@"%@/%d",@"1",_totalCount];
-    //添加定时器
-    if (_timer) {
-        [_timer timeInterval];
-        _timer = nil;
-    }
-    else
-    {
-        _timer =   [NSTimer scheduledTimerWithTimeInterval:TimeInterval target:self selector:@selector(ScrollNext) userInfo:nil repeats:YES];
+    _imgCountLB.text = [NSString stringWithFormat:@"%@/%d",@"0",_totalCount];
+
+    if (ListAry.count > 0 ) {
+        if (ListAry.count > 1) {
+            [self addTimer];//增加计时器
+        }
+        _imgCountLB.text = [NSString stringWithFormat:@"%@/%d",@"1",_totalCount];
 
     }
 }
@@ -106,6 +110,31 @@
         [self.delegate touchesEventBYIndex:sender.tag - 100];
     }
 }
+/**
+ *  添加定时器
+ */
+- (void)addTimer
+{
+    //添加定时器
+    if (_timer) {
+        [_timer timeInterval];
+        _timer = nil;
+    }
+    else
+    {
+        _timer =   [NSTimer scheduledTimerWithTimeInterval:TimeInterval target:self selector:@selector(ScrollNext) userInfo:nil repeats:YES];
+        
+    }
+
+}
+/**
+ *  取消定时器
+ */
+- (void)cancelTimer
+{
+    [_timer timeInterval];
+    _timer = nil;
+}
 
 /**
  *  定时器调用的方法
@@ -122,13 +151,15 @@
         offSet = self.frame.size.width;
     }
     point.x += offSet;
-    [UIView animateWithDuration:TimeInterval animations:^{
+    [UIView animateWithDuration:0.5 animations:^{
         self.imgSV.contentOffset = point;
     }];
     
 }
-
-
+- (void)dealloc
+{
+    [self cancelTimer];
+}
 
 
 
